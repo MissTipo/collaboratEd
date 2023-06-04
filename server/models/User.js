ff96a5bdf170b0aaf295d8daa3c2d3f77c8db928
apha-pesso <mutiuayo99@gmail.com>
2023-06-04 14:00:20 +0100

    User model and storage definition

---
+++ server/models/User.js
@@ -0,0 +1,40 @@
+// Creating the basic User schema
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true, // this will make sure that the email is unique
+  },
+
+  phone: {
+    type: Number,
+    required: false,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
---
+++ server/utils/db.js
@@ -0,0 +1,31 @@
+// The database connection and the user model are exported from the file.
+
+const mongoose = require('mongoose');
+const User = require('../models/User');
+
+class Database {
+  constructor() {
+    // this.options = options ? options : {};
+    // create connection to the database
+    mongoose.connect('mongodb://localhost/mytest', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    this.db = mongoose.connection;
+    this.db.on('error', console.error.bind(console, 'connection error:'));
+    this.db.once('open', () => {
+      console.log('Connected to DB');
+    });
+
+    // the user model
+    this.User = User;
+  }
+
+  // close the database connection
+  closeConnection() {
+    this.db.close();
+  }
+}
+
+const db = new Database();
+module.exports = db;
