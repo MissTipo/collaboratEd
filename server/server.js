50c37024c2e655904515e538cb9356b1106e6a08
MissTipo <dorine.a.tipo@gmail.com>
2023-06-05 07:15:29 +0300

    Establish mongo atlas connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5050;
@@ -9,12 +9,12 @@ const port = process.env.PORT || 5050;
 app.use(cors());
 app.use(express.json());
 
-/* const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established")
-}) */
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
