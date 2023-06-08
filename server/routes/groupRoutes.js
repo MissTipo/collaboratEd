11c5ab1b0f776f4e3f76bb6680307972b54ba192
apha-pesso <mutiuayo99@gmail.com>
2023-06-08 11:31:54 +0100

    User create and join group

--- server/routes/groupRoutes.js
+++ server/routes/groupRoutes.js
@@ -1,27 +1,48 @@
 const express = require('express');
+
 const router = express.Router();
 const groupController = require('../controllers/groupController');
+const authController = require('../controllers/authController');
 
-// Create a new group
-router.post('/group', groupController.createGroup);
+// Create a new group by a verified user
+router.post(
+  '/group',
+  authController.authenticateUser,
+  groupController.createGroup,
+);
 
 // Retrieve all groups
 router.get('/groups', groupController.getGroups);
 
 // Retrieve a single group with groupId
-router.get('/group/:id', groupController.getGroupById);
+router.get(
+  '/group/:id',
+  authController.authenticateUser,
+  groupController.getGroupById,
+);
 
-// Update a group with groupId
-router.put('/group/:id', groupController.updateGroup);
+// Update a group with groupId by verified user
+router.put(
+  '/group/:id',
+  authController.authenticateUser,
+  groupController.updateGroup,
+);
 
-// Delete a group with groupId
-router.delete('/group/:id', groupController.deleteGroup);
+// Delete a group with groupId by verified user
+router.delete(
+  '/group/:id',
+  authController.authenticateUser,
+  groupController.deleteGroup,
+);
 
 // Add a resource to group's library
 router.post('/group/:id/resource', groupController.addResourceToLibrary);
 
 // Remove a resource from group's library
-router.delete('/group/:id/resource/:resourceId', groupController.deleteResource);
+router.delete(
+  '/group/:id/resource/:resourceId',
+  groupController.deleteResource,
+);
 
 // Get all resources from group's library
 router.get('/group/:id/resources', groupController.getResources);
