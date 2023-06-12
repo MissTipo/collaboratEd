e8bf7297b472de2e57e764cd2182764c35136f9c
apha-pesso <mutiuayo99@gmail.com>
2023-06-12 21:48:53 +0100

    test

--- client/collabFrontEnd/src/components/Login/Login.jsx
+++ client/collabFrontEnd/src/components/Login/Login.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
-import { FiUser, FiLock } from 'react-icons/fi';
-import './Login.css';
+import React from "react";
+import { FiUser, FiLock } from "react-icons/fi";
+import "./Login.css";
+import request from "request";
+// import { request } from "../../utils/request";
 
 const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Call to API to log in user
+
+    // fetch("http://0.0.0.0:5050/api/login", {
+    //   method: "GET",
+    //   body: JSON.stringify({
+    //     username: e.target.username.value,
+    //     password: e.target.password.value,
+    //   }),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then((res) => {
+    //     if (res.status === 200) {
+    //       // displY BODY
+    //       console.log(res.body);
+
+    //       // Redirect to dashboard
+    //       window.location.href = "/dashboard";
+    //     } else {
+    //       // Display error message
+    //       console.log("Error logging in");
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    // request("/login", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     username: e.target.username.value,
+    //     password: e.target.password.value,
+    //   }),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then((res) => {
+    //     if (res.status === 200) {
+    //       // Redirect to dashboard
+    //       window.location.href = "/dashboard";
+    //     } else {
+    //       // Display error message
+    //       console.log("Error logging in");
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
     // Handle login logic here
   };
 
