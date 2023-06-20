ce2a731593dea0a8994da0cd3fe42bb66f27c480
MissTipo <dorine.a.tipo@gmail.com>
2023-06-20 18:49:00 +0300

    voice channel controller

--- server/controllers/voiceChannelController.js
+++ server/controllers/voiceChannelController.js
@@ -7,7 +7,12 @@ const { initiateCall } = require('../utils/webrtc');
 // create voice channel
 const createVoiceChannel = async (req, res) => {
   try {
-    const voiceChannel = await VoiceChannel.create(req.body);
+    const { name, channelType } = req.body;
+    const voiceChannel = await VoiceChannel.create({
+      name: name,
+      channelType: channelType,
+      group: req.params.groupId
+    });
     res.status(201).json(voiceChannel);
     voiceChannel.save();
   } catch (error) {
@@ -41,6 +46,54 @@ const joinVoiceChannel = async (req, res) => {
   }
 }
 
+// Share screen in voice channel
+const shareScreen = async (req, res) => {
+  try {
+    const { channelId, userId } = req.body;
+    const channel = await voiceChannel.findById(channelId);
+    if (!channel) {
+      return res.status(404).json({ message: 'Channel not found!' });
+    }
+
+    // Check if user is a participant of the channel
+    if (!channel.participants.includes(userId)) {
+      return res.status(401).json({ message: 'You are not a participant of this channel!' });
+    }
+
+    // Initiate screen sharing for the channel
+    initiateScreenShare(channel);
+
+    // Emit a socket event to notify other group members that a user has started screen sharing
+    io.emit('userStartedScreenShare', { channelId, userId });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
+// leave voice channel
+const leaveVoiceChannel = async (req, res) => {
+  try {
+    const { channelId, userId } = req.body;
+    const channel = await voiceChannel.findById(channelId);
+    if (!channel) {
+      return res.status(404).json({ message: 'Channel not found!' });
+    }
+
+    // Remove user from channel
+    channel.participants = channel.participants.filter((id) => id !== userId);
+    await channel.save();
+
+    // Emit a socket event to notify other group members that a user has left the channel
+    io.emit('userLeftChannel', { channelId, userId });
+
+    // return the channel
+    res.status(200).json(channel);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
 // get all voice channels
 const getAllVoiceChannels = async (_, res) => {
   try {
@@ -65,4 +118,4 @@ const getVoiceChannelById = async (req, res) => {
   }
 }
 
-module.exports = { createVoiceChannel, getAllVoiceChannels, getVoiceChannelById, joinVoiceChannel };
+module.exports = { createVoiceChannel, getAllVoiceChannels, getVoiceChannelById, joinVoiceChannel, leaveVoiceChannel, shareScreen };
