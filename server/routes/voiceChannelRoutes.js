415b411100b3c299557172d3feff26e09fd691ff
MissTipo <dorine.a.tipo@gmail.com>
2023-06-21 11:20:18 +0300

    schema, api, routes, webrtc implementation

---
+++ server/routes/voiceChannelRoutes.js
@@ -0,0 +1,15 @@
+// Contains the routes for the voice channel endpoints
+const express = require('express');
+const router = express.Router();
+const voiceChannelController = require('../controllers/voiceChannelController');
+
+// Create a new voice channel
+router.post('/create-channel', voiceChannelController.createVoiceChannel);
+
+// Join a voice channel
+router.post('/voice-channel/join', voiceChannelController.joinVoiceChannel);
+
+// Get all voice channels
+router.get('/get-channels', voiceChannelController.getAllVoiceChannels);
+
+module.exports = router;
